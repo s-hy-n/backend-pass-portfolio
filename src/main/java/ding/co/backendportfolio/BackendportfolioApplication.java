baaa12a0b679e2e42a405cdaea59ef5b1cf7de85
hyuniverse <hyuniverse12@gmail.com>
2025-08-20 11:08:40 +0900

    test commit

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        System.out.println("✅Backend Portfolio Application is starting...");
+        System.out.println("✅Backend Portfolio Application is starting!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 }
